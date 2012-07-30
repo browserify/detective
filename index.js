1d279b2e3c0cd7cb33033d7ced3a5920cb1ea8ce
James Halliday <mail@substack.net>
2012-07-30 07:37:12 -0700

    slow pass to pick off expression strings

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ var traverse = function (node, cb) {
     }
     else if (node && typeof node === 'object') {
         cb(node);
+        
         Object.keys(node).forEach(function (key) {
             traverse(node[key], cb);
         });
     }
 };
 
-var walk = function (src, cb, opts) {
-    var ast = esprima.parse(src.toString());
+var walk = function (src, cb) {
+    var ast = esprima.parse(src);
+    traverse(ast, cb);
+};
+
+var walkSlow = function (src, cb) {
+    var ast = esprima.parse(src, { range : true });
     traverse(ast, cb);
 };
 
@@ -26,26 +32,43 @@ var exports = module.exports = function (src, opts) {
 exports.find = function (src, opts) {
     if (!opts) opts = {};
     var word = opts.word === undefined ? 'require' : opts.word;
+    if (typeof src !== 'string') src = String(src);
     
-    var modules = { strings : [], expressions : [] };
-    
-    if (src.toString().indexOf(word) == -1) return modules;
-    
-    walk(src, function (node) {
-        var isRequire =
-            node.type === 'CallExpression'
+    function isRequire (node) {
+        return node.type === 'CallExpression'
             && node.callee.type === 'Identifier'
             && node.callee.name === word
         ;
-        if (isRequire) {
-            if (node.arguments.length && node.arguments[0].type === 'Literal') {
-                modules.strings.push(node.arguments[0].value);
-            }
-            else {
-                modules.expressions.push('...');
-            }
+    }
+    
+    var modules = { strings : [], expressions : [] };
+    
+    if (src.indexOf(word) == -1) return modules;
+    
+    var slowPass = false;
+    
+    walk(src, function (node) {
+        if (!isRequire(node)) return;
+        if (node.arguments.length
+        && node.arguments[0].type === 'Literal') {
+            modules.strings.push(node.arguments[0].value);
+        }
+        else {
+            slowPass = true;
         }
     });
+    
+    if (slowPass) {
+        walkSlow(src, function (node) {
+            if (!isRequire(node)) return;
+            if (!node.arguments.length
+            || node.arguments[0].type !== 'Literal') {
+                var r = node.arguments[0].range;
+                var s = src.slice(r[0], r[1] + 1);
+                modules.expressions.push(s);
+            }
+        });
+    }
     
     return modules;
 };
